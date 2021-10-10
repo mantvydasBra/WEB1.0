ca649c5a388b7db44d4e5dfdbfe4006f04282f9c
Mantvydas Bražėnas <mantvydasbr7@gmail.com>
2021-10-10 14:23:18 +0300

    added text, made page better

--- main.js
+++ main.js
@@ -9,3 +9,16 @@ document.getElementById("menu").addEventListener("click", function(e) {
         e.target.classList.add("active");
     }
 });
+
+function zoomIn(imgs) {
+    // Get the expanded image
+    var expandImg = document.getElementById("expandedImg");
+    // Get the image text
+    var imgText = document.getElementById("imgtext");
+    // Use the same src in the expanded image as the image being clicked on from the grid
+    expandImg.src = imgs.src;
+    // Use the value of the alt attribute of the clickable image as text inside the expanded image
+    imgText.innerHTML = imgs.alt;
+    // Show the container element (hidden with CSS)
+    expandImg.parentElement.style.display = "block";
+  }
